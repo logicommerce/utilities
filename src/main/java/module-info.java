f56fb0db3a18f5893fdf004a7b1ba8045be198b2
Francesc Requesens <francesc.requesens@logicommerce.com>
2022-05-24 15:20:41 +0200

    Improved Jackson accessibility

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.logicommerce.utilities {
 	requires transitive java.logging;
 	requires transitive com.fasterxml.jackson.core;
 	requires transitive com.fasterxml.jackson.databind;
-	
+	requires transitive com.fasterxml.jackson.annotation;
+
 	exports com.logicommerce.utilities;
 	exports com.logicommerce.utilities.builders;
 	exports com.logicommerce.utilities.annotations;
